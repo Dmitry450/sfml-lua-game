9317fcb301dc77b300043612a6f0cdd7c97092e5
indev <bdmitry450@gmail.com>
2021-08-15 18:58:14 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
 #include <SFML/Graphics.hpp>
+#include <lua.hpp>
 #include "camera.hpp"
 #include "world.hpp"
 #include "entitymgr.hpp"
 #include "entity.hpp"
 #include "animation.hpp"
 #include "stdroutines.hpp"
+#include "resource_holder.hpp"
+#include "lua_api.hpp"
 #include <stdint.h>
 #include <iostream>
 
 Camera camera = { 0, 0 };
+
+World *world = nullptr;
+EntityManager *entmgr = nullptr;
+TextureHolder *textures = nullptr;
 
 int main()
 {
@@ -30,8 +37,15 @@ int main()
                             2, 1, 1, 1, 1, 1, 1, 2,
                             2, 2, 2, 2, 2, 2, 2, 2, };
     
-    World world(worlddata, width, height);
-    EntityManager entmgr;
+    world = new World(worlddata, width, height);
+    entmgr = new EntityManager;
+    textures = new TextureHolder;
+    
+    lua_State *L = luaL_newstate();
+    
+    api_init(L);
+    
+    run_script(L, "scripts/test.lua");
     
     // BEGIN TEST PLAYER CODE
     PlayerParams params;
@@ -43,20 +57,17 @@ int main()
     player->setSize(32, 32);
     player->setPosition(40, 40);
     
-    sf::Texture texture;
-    texture.loadFromFile("resources/char.png");
-    player->setTexture(&texture);
+    textures->loadResource("resources/char.png");
+    player->setTexture(textures->getResource("resources/char.png"));
     
-    Animation walk_right(0.2, {sf::IntRect(0, 0, 32, 32), sf::IntRect(32, 0, 32, 32), sf::IntRect(64, 0, 32, 32)});
-    Animation walk_left(0.2, {sf::IntRect(32, 0, -32, 32), sf::IntRect(64, 0, -32, 32), sf::IntRect(96, 0, -32, 32)});
+    Animation walk(0.2, {sf::IntRect(0, 0, 32, 32), sf::IntRect(32, 0, 32, 32), sf::IntRect(64, 0, 32, 32)});
     
     AnimationManager playeranims;
-    playeranims.addAnimation("walk_left", &walk_left);
-    playeranims.addAnimation("walk_right", &walk_right);
+    playeranims.addAnimation("walk", &walk);
     
     player->animations = playeranims;
     
-    entmgr.addEntity(player);
+    entmgr->addEntity(player);
     
     // END TEST PLAYER CODE
     
@@ -87,16 +98,18 @@ int main()
         camera.x = playerpos.x - 400;
         camera.y = playerpos.y - 300;
         
-        entmgr.update(dtime, world);
+        entmgr->update(dtime, *world);
+        
+        run_update_hooks(L, dtime);
         
         run_fall_physics(player, dtime, 320);
         run_player_controls(player, dtime, params);
         
         window.clear(sf::Color(40, 79, 23, 128));
         
-        window.draw(world);
+        window.draw(*world);
         
-        entmgr.draw(window);
+        entmgr->draw(window);
         
         window.display();
     }
